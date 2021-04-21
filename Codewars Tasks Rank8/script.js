4f3cd6431218f95780c8f72fbaa69920c6be53e9
pavel <paul8paul8@mail.ru>
2021-04-21 22:45:44 +0300

    Update script.js

--- Codewars Tasks Rank8/script.js
+++ Codewars Tasks Rank8/script.js
@@ -58,3 +58,21 @@ function rentalCarCost(d) {
 	  return cost * d - 20;
 	} else return cost * d;
 }
+// 5. You are going to be given a word. Your job is to return the middle character of the word. 
+// If the word's length is odd, return the middle character. 
+// If the word's length is even, return the middle 2 characters.
+
+// #Examples:
+// Kata.getMiddle("test") should return "es"
+// Kata.getMiddle("testing") should return "t"
+// Kata.getMiddle("middle") should return "dd"
+// Kata.getMiddle("A") should return "A"
+
+function getMiddle(s) {
+	let mid = Math.floor(s.length / 2)
+	if (s.length % 2 == 0) {
+	  return s[mid - 1] + s[mid];
+	} else {
+	  return s[mid];
+	}
+}
